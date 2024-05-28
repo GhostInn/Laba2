fbfec856e253a9249bd084bb34b2538337e44690
GhostInn <matyuninvladislav1@gmail.com>
2024-05-28 16:50:13 +0300

    t

--- lab2/RegattaExtensions.cs
+++ lab2/RegattaExtensions.cs
@@ -1,6 +1,38 @@
-namespace lab2;
-
-public class RegattaExtensions
+// Определяем пространство имен для класса RegattaExtensions
+namespace lab2
 {
-    
+    // Определяем статический класс RegattaExtensions, который содержит методы расширения для класса Regatta
+    public static class RegattaExtensions
+    {
+        // Метод расширения Winners для класса Regatta, который возвращает новый экземпляр класса Regatta,
+        // содержащий первые три яхты из экземпляра regatta, отсортированные по убыванию их скорости
+        public static Regatta Winners(this Regatta regatta)
+        {
+            // Создаем переменную sortedYachts, которая содержит отсортированный список яхт из экземпляра regatta
+            // в порядке убывания их скорости. Используем метод расширения OrderByDescending для класса IEnumerable<T>
+            var sortedYachts = regatta.OrderByDescending(yacht => yacht.Speed);
+
+            // Возвращаем новый экземпляр класса Regatta, который содержит первые три яхты из отсортированного списка
+            // sortedYachts. Используем метод расширения Take для класса IEnumerable<T>
+            return new Regatta(sortedYachts.Take(3));
+        }
+
+        // Метод расширения SlowestYachtSpeedToDraftRatio для класса Regatta, который вычисляет отношение скорости
+        // к осадке для самого медленного корабля в экземпляре regatta
+        public static double SlowestYachtSpeedToDraftRatio(this Regatta regatta)
+        {
+            // Создаем переменную slowestYacht, которая содержит самый медленный корабль из экземпляра regatta.
+            // Используем метод расширения MinBy для класса IEnumerable<T>
+            var slowestYacht = regatta.MinBy(yacht => yacht.Speed);
+
+            // Если переменная slowestYacht равна null, то возвращаем 0
+            if (slowestYacht == null)
+            {
+                return 0;
+            }
+
+            // Вычисляем отношение скорости к осадке для самого медленного корабля и возвращаем его из метода
+            return slowestYacht.Speed / slowestYacht.Draft;
+        }
+    }
 }
